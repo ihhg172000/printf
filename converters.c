6d7273ce6808f8f63e7e78de56bc2d2cdb76e557
yousef <yabdelshafy0@gmail.com>
2023-04-25 00:40:05 +0200

    add converters.c file

---
+++ converters.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* handle_converters - _
+* @s: _
+*
+* Return: _
+*/
+void (*handle_converters(char s))(buff_t *buff, va_list l)
+{
+	int i;
+
+	converter_t converters[] = {
+		{'%', convert_precent},
+		{'c', convert_c},
+		{'s', convert_s},
+		{'\0', NULL}
+	};
+
+	for (i = 0; converters[i].s; i++)
+	{
+		if (converters[i].s == s)
+			return (converters[i].f);
+	}
+
+	return (NULL);
+}
