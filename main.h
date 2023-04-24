4fd7088417ba528d8a6350ea74ed6d08b43db560
Islam Hany <ihhg.developer@gmail.com>
2023-04-24 23:57:58 +0200

    add main.h header file

---
+++ main.h
@@ -0,0 +1,50 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#define BUFFSIZE 1024
+
+/**
+* struct buffer - _
+* @b: _
+* @p: _
+* @s: _
+* @o: _
+*/
+typedef struct buffer
+{
+	char b[BUFFSIZE];
+	char *p;
+	int s;
+	int o;
+} buff_t;
+
+/**
+* struct converter - _
+* @s: _
+* @f: _
+*/
+typedef struct converter
+{
+	char s;
+	void (*f)(buff_t *buff, va_list l);
+} converter_t;
+
+int _printf(const char *format, ...);
+
+void (*handle_converters(char specifier))(buff_t *buff, va_list l);
+
+void convert_precent(buff_t *buff, va_list l);
+void convert_c(buff_t *buff, va_list l);
+void convert_s(buff_t *buff, va_list l);
+
+void clean_buffer(buff_t *buff);
+void write_buffer(buff_t *buff);
+int validate_buffer(buff_t *buff, int rs);
+void handle_buffer_s(buff_t *buff, char *s);
+void handle_buffer_c(buff_t *buff, char c);
+
+#endif
