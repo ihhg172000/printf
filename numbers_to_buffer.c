33122b2f1beb4e07c23b4c84c553ee37d19a879d
Islam Hany <ihhg.developer@gmail.com>
2023-04-25 20:20:23 +0200

    add digit_to_char function

--- numbers_to_buffer.c
+++ numbers_to_buffer.c
@@ -1,6 +1,37 @@
 #include "main.h"
 
 /**
+* digit_to_char - _
+* @n: _
+*/
+char digit_to_char(int n)
+{
+	switch (n)
+	{
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+			return (n + 48);
+		case 10:
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+			return (n + 65);
+		default:
+			return (0);
+	}
+}
+
+/**
 * unsigned_int_to_buffer- _
 * @buff: _
 * @n: _
@@ -12,8 +43,8 @@ void unsigned_int_to_buffer(buff_t *buff, unsigned int n, unsigned int base)
 		unsigned_int_to_buffer(buff, n / base, base);
 	else
 		return;
-
-	handle_buffer_c(buff, (n % base) + 48);
+	
+	handle_buffer_c(buff, digit_to_char(n % base));
 }
 
 /**
