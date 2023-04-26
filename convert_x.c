076da5078244d9196f61645b8235141f2e92a57c
yousef <yabdelshafy0@gmail.com>
2023-04-26 02:01:25 +0200

    add convert_x.c file

---
+++ convert_x.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* convert_x - _
+* @buff: _
+* @l: _
+*/
+void convert_x(buff_t *buff, va_list l)
+{
+	unsigned int n = va_arg(l, unsigned int);
+
+	unsigned_int_to_buffer(buff, n, 16, a);
+}
